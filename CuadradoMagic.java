27909f282bb74c50956b792cddc7e12e6cc7ed34
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-19 16:21:22 -0400

    Definición de funciones y avance de código

--- CuadradoMagic.java
+++ CuadradoMagic.java
@@ -1,26 +1,32 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 class CuadradoMagic {
     public static void main(String[] args) {
         Integer[][] cuadradoMagico = new Integer[3][3];
         llenarCuadrado(cuadradoMagico);
-        // verificarCuadrado(cuadrado);
+        verificarCuadrado(cuadradoMagico);
     }
 
-    public static void llenarCuadrado(Integer[][] m) {
+    public static int[][] llenarCuadrado(int[][] m) {
         Random azar = new Random();
         for (int fila = 0; fila < 3; fila++) {
             for (int columna = 0; columna < 3; columna++) {
                 m[fila][columna] = azar.nextInt(9) + 1;
             }
         }
+        return m;
     }
 
-    public static boolean verificarCuadrado(Integer[][] m) {
+    public static boolean verificarCuadrado(int[][] m) {
         boolean esMagico = false;
-        for (int fila = 0; fila < 2; fila++) {
+        for (int fila = 0; fila < 3; fila++) {
             if (m[fila][0] + m[fila][1] + m[fila][2] == 15) {
-                esMagico = true;
+                if (m[0][0] + m[1][1] + m[2][2] == 15) {
+                    if (m[0][2] + m[1][1] + m[2][0]) {
+                        esMagico = true;
+                    }
+                }
             }
         }
         return esMagico;
